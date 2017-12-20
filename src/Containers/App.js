0fffe8747ad301630f3ca2c1fcd9b52d297da669
Ian Nicholas <imnich91@gmail.com>
2017-12-19 21:43:00 -0500

    bundle

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Switch, Route } from 'react-router-dom';
-
+import HomeContainer from './HomeContainer';
 
 
 class App extends React.Component {
